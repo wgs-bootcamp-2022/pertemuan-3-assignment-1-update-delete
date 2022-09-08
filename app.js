be6d0fa11a69da6e679865c82ff2f443d6b06ada
zakariyahya <yazakkars@gmail.com>
2022-09-08 10:20:00 +0700

    assignment 1 update delete

---
+++ app.js
@@ -0,0 +1,91 @@
+const { command, demandOption } = require("yargs");
+const yargs = require("yargs");
+const { saveContact, listContact, getDetailContact, deleteContact, updateContact } = require("./functions");
+
+yargs.command({
+    command:"add",
+    describe:"Add Contact",
+    builder:{
+        name:{
+            desciribe:"Contact Name",
+            demandOption:true,
+            type:"string"
+        },
+        number:{
+            desciribe:"Contact Number",
+            demandOption:true,
+            type:"string"
+        },
+        email:{
+            desciribe:"Contact Email",
+            demandOption:false,
+            type:"string"
+        }
+    },
+    handler(argv){
+        saveContact(argv.name,argv.number,argv.email)
+    }
+})
+yargs.command({
+    command:'list',
+    describe:'see contact list',
+    handler(){
+        listContact()
+    }
+})
+
+yargs.command({
+    command:'detail',
+    describe:"See detail contact",
+    builder:{
+        name:{
+            desciribe:"Contact Name",
+            demandOption:true,
+            type:"string"
+        }
+    },
+    handler(argv){
+        getDetailContact(argv.name)
+    }
+})
+
+yargs.command({
+    command:'delete',
+    describe:"Delete Contact",
+    builder:{
+        number:{
+            desciribe:"Contact Name",
+            demandOption:true,
+            type:"string"
+        }
+    },
+    handler(argv){
+        deleteContact(argv.number)
+    }
+})
+
+yargs.command({
+    command:"update",
+    describe:"Update Contact",
+    builder:{
+        name:{
+            desciribe:"Contact Name",
+            demandOption:true,
+            type:"string"
+        },
+        number:{
+            desciribe:"Contact Number",
+            demandOption:true,
+            type:"string"
+        },
+        email:{
+            desciribe:"Contact Email",
+            demandOption:false,
+            type:"string"
+        }
+    },
+    handler(argv){
+        updateContact(argv.name,argv.number,argv.email)
+    }
+})
+yargs.parse()
---
+++ functions.js
@@ -0,0 +1,86 @@
+const fs = require('fs')
+const { default: validator } = require('validator')
+const filepath = './data/contact.json'
+const existFolder = fs.existsSync('./data')
+const existFile = fs.existsSync(filepath)
+
+if (!existFolder){
+    fs.mkdirSync('./data')
+}
+
+if(!existFile){
+    fs.writeFileSync(filepath,'[]')
+}
+
+const saveContact = (name, number, email)=> {
+    const contact = {name, number, email}
+    const file = fs.readFileSync(filepath, 'utf-8')
+    const contacts = JSON.parse(file)
+
+    const duplicate = contacts.find((cont) => cont.name === name)
+    const duplicateEmail = contacts.find((cont) => cont.email === email)
+    const duplicateNumber = contacts.find((cont) => cont.number === number)
+
+    const valEmail = validator.isEmail(email)
+    const valNumber = validator.isMobilePhone(number,'id-ID')
+
+    if(duplicate){
+        console.log('Contact Name is already recorded')
+        return false
+    }
+
+    if(!email || valEmail==false || duplicateEmail){
+        console.log('Email format is not valid or Contact Email is already recorded')
+        return false
+    }
+
+    if(valNumber==false || duplicateNumber){
+        console.log('Mobile Number format is not valid or Contact Number is already recorded')
+        return false
+    }
+
+    contacts.push(contact)
+    fs.writeFileSync(filepath, JSON.stringify(contacts))
+}
+
+const getAllContact = ()=> {
+    const file = fs.readFileSync(filepath,'utf-8')
+    const contacts = JSON.parse(file)
+    return contacts
+}
+
+const listContact = ()=>{
+    const contacts = getAllContact()
+    contacts.forEach((contact,i)=>{
+        console.log(`${i+1}.${contact.name} - ${contact.number}`)
+    })
+}
+
+const getDetailContact = (name) => {
+    const contacts = getAllContact()
+    console.log('Detail Contact')
+    const detailContact = contacts.find((cont) => cont.name === name)
+    console.log(`${detailContact.name}\n${detailContact.number}\n${detailContact.email}`)
+}
+
+const deleteContact = (number) => {
+    const contacts = getAllContact()
+    let deleteContact = contacts.filter(user => user.number != number);
+    fs.writeFileSync(filepath, JSON.stringify(deleteContact))
+    console.log('Contact telah terhapus')
+}
+
+const updateContact = (name,number,email) => {
+    const file = fs.readFileSync(filepath, 'utf-8')
+    const contacts = JSON.parse(file)
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].number === number ) {
+            contacts[i].name = name
+            contacts[i].number = number;
+            contacts[i].email = email;
+        }
+      }
+    fs.writeFileSync(filepath, JSON.stringify(contacts))
+}
+
+module.exports = {saveContact, listContact, getDetailContact, deleteContact, updateContact}
